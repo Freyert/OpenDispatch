fc36a198cf69fc322c032f9861ebf4043d02df84
Fulton Byrne <fulton.byrne@gmail.com>
2015-03-02 22:45:13 -0500

    Markers render on MapView!

--- static/backbone/dispatchApp/index.js
+++ static/backbone/dispatchApp/index.js
@@ -7,6 +7,7 @@ var MapView = require('./views/mapView');
 var ListView = require('./views/listView');
 var Rides = require('./collections/rides');
 $(document).ready(function() {
-  var mapView = new MapView();
-  var listView = new ListView({collection: new Rides()});
+  var rides = new Rides(); //Every view shares the same collection.
+  var mapView = new MapView({collection: rides});
+  var listView = new ListView({collection: rides});
 });
--- static/backbone/dispatchApp/views/mapView.js
+++ static/backbone/dispatchApp/views/mapView.js
@@ -7,15 +7,35 @@ GoogleMapLoader.VERSION = '3.14';
 module.exports = Backbone.View.extend({
   el: "body",
   initialize: function () {
-    GoogleMapLoader.load(function(google) {
+    var init = function(google) {
       var mapOptions = { 
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8
       };
 
       var map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);
+        mapOptions);
+      this.render = this.render.bind(this, google, map);
       //google.map.event.addDomListener(window, 'load', initialize);
+      this.listenTo(this.collection, 'change', this.render);
+      //This here is a hack to get it to work now.
+      //TODO: Collections should be preloaded.
+      this.collection.fetch();
+      this.render();
+    }.bind(this);
+    GoogleMapLoader.load(init);
+  },
+
+  render: function(google, map) {
+    this.collection.each(function(ride) {
+      var latitude = ride.get("start").latitude;
+      var longitude = ride.get("start").longitude;
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latitude, longitude),
+        map: map,
+        title: ride.get("firstName")
+      });
+      console.log(marker);
     });
   }
 });
